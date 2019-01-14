aa51516bcb573a763364b19e544aa7db22bce06c
pratyakash <pratyakash13@gmail.com>
2019-01-14 14:27:09 +0530

    ToolBar

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ class App extends Component {
           padding:'10px 0px'
         }}
       >
-        header
+        <Toolbar>
+          <div className="header_logo">
+            <div className=" font_righteous header_logo_venue">The Venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+        </Toolbar>
       </AppBar>
     );
   }

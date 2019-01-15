2dac7710a5674a7f96da8f5d474e2e2d7f2c7483
pratyakash <pratyakash13@gmail.com>
2019-01-15 19:37:45 +0530

    Countdown animation

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import Slide from 'react-reveal/Slide';
 
 const TimeUntil = () => {
 
 
     return (
-        <div className="countdown_wrapper">
-            <div className="countdown_top">
-                Event Starts In
-            </div>
+        <Slide left delay={1000}>
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event Starts In
+                </div>
 
-            <div className="countdown_bottom">
-                <div className="countdown_items">
-                    <div className="countdown_time">
-                        23
+                <div className="countdown_bottom">
+                    <div className="countdown_items">
+                        <div className="countdown_time">
+                            23
                     </div>
-                    <div className="countdown_tag">
-                        Days
+                        <div className="countdown_tag">
+                            Days
                     </div>
-                    
+                    </div>
+                    <div className="countdown_items">
+                        <div className="countdown_time">
+                            23
+                    </div>
+                        <div className="countdown_tag">
+                            Hours
+                    </div>
+                    </div>
+                    <div className="countdown_items">
+                        <div className="countdown_time">
+                            23
+                    </div>
+                        <div className="countdown_tag">
+                            MIN
+                    </div>
+                    </div>
+                    <div className="countdown_items">
+                        <div className="countdown_time">
+                            23
+                    </div>
+                        <div className="countdown_tag">
+                            SEC
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Slide>
+        
     );
 };
 

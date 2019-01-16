99a9fbf6178947b54d9180e699a1838e10dc73e1
pratyakash <pratyakash13@gmail.com>
2019-01-16 13:47:15 +0530

    Updates

---
+++ precache-manifest.f49655d4576f80a8b85925e01bf3ea7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "a2c9cb2b5d2d2e65ac63",
+    "url": "/venue/static/js/runtime~main.a2c9cb2b.js"
+  },
+  {
+    "revision": "1ecabc835ed8f3fb59a4",
+    "url": "/venue/static/js/main.1ecabc83.chunk.js"
+  },
+  {
+    "revision": "b710be2d34688b02df73",
+    "url": "/venue/static/js/1.b710be2d.chunk.js"
+  },
+  {
+    "revision": "1ecabc835ed8f3fb59a4",
+    "url": "/venue/static/css/main.2e21e079.chunk.css"
+  },
+  {
+    "revision": "0d4ddfeadbc588a6afa5e62e0210ed9f",
+    "url": "/venue/index.html"
+  }
+];
